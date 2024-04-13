c79c085df604d536f64f6ce022e23153d68bc1f8
julianatonu <julianatonu@gmail.com>
2024-04-13 21:53:49 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { router } from './router/Router'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-   
-   <div>
-    <h1>Hellooooo</h1>
-   </div>
+
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   )
 }
 
