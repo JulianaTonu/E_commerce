4c7c302eb7c271591722e6c31423279340737f48
julianatonu <julianatonu@gmail.com>
2024-04-17 00:04:27 +0600

    coverWmr

--- src/components/Womens/Womens.jsx
+++ src/components/Womens/Womens.jsx
@@ -1,8 +1,11 @@
 
+import coverWomen from '../../assets/womens/coverWm.jpg'
 const Womens = () => {
     return (
         <div>
-            
+            <div>
+    <img src={coverWomen} alt="" className='w-full lg:h-96'/>
+</div>
         </div>
     );
 };
