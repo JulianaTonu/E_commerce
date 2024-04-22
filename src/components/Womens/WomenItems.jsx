0b7059edfcb820efc5403f4b50494eee643d441d
julianatonu <julianatonu@gmail.com>
2024-04-22 15:28:54 +0600

    womenitems

--- src/components/Womens/WomenItems.jsx
+++ src/components/Womens/WomenItems.jsx
@@ -7,8 +7,21 @@ const WomenItems = ({ id, name, image, oldPrice,newPrice}) => {
         <figure className=" ">
             <Link to={`/Womendetails/${id}`}><img src={image} alt="" className="rounded-md border bg-slate-300 h-80 w-80 hover:scale-125 transition-all duration-500 cursor-pointer" /></Link>
         </figure>
-      
-      
+        <div className="p-2 ms-4  text-center">
+            <h2 className="font-bold text-xl">{name}</h2>
+            <div className="rating">
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            </div>
+            <div className="flex gap-4 text-center justify-center">
+                <p className="text-lg">{newPrice}</p>
+                <p className="text-[#da8214] bold font-semibold line-through mt-1">{oldPrice}</p>
+            </div>
+
+        </div>
     </div>
     );
 };
