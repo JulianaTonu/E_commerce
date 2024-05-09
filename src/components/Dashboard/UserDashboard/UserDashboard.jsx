947d03b1336533875f741fb2cbd14286931f29b4
julianatonu <julianatonu@gmail.com>
2024-05-09 12:30:58 +0600

    Home

--- src/components/Dashboard/UserDashboard/UserDashboard.jsx
+++ src/components/Dashboard/UserDashboard/UserDashboard.jsx
@@ -1,60 +1,111 @@
+import  { useState, useEffect } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../../provider/AuthProvider";
-import {FaAddressBook } from "react-icons/fa";
+import { FaAddressBook } from "react-icons/fa";
 import { MdHome } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 
+function padZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
 
 const UserDashboard = () => {
-    const { user } = useContext(AuthContext)
-    return (
+  const { user } = useContext(AuthContext);
+  const [countdown, setCountdown] = useState({ days: 10, hours: 9, minutes: 3, seconds: 0 });
 
-        <div>
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      setCountdown(prevCountdown => {
+        let { days, hours, minutes, seconds } = prevCountdown;
+        seconds--;
+        if (seconds < 0) {
+          seconds = 59;
+          minutes--;
+          if (minutes < 0) {
+            minutes = 59;
+            hours--;
+            if (hours < 0) {
+              hours = 23;
+              days--;
+              if (days < 0) {
+                clearInterval(countdownInterval);
+                return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+              }
+            }
+          }
+        }
+        return { days, hours, minutes, seconds };
+      });
+    }, 1000);
 
-            <div className="flex items-center ">
-                <h2 className="me-3 text-2xl">Hi, Welcome  </h2>
-                <p className="text-4xl capitalize text-orange-600">{
-                    user?.displayName ? user.displayName : ''
-                }</p>
-            </div>
+    return () => clearInterval(countdownInterval);
+  }, []);
 
-            <div className="flex gap-3 my-5">
-                <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-purple-200 flex text-center items-center ">
-                    <div className="w-1/2 ps-14">
-                        <p className="text-5xl mt-3"><FaAddressBook /></p>
-                    </div>
-                    <div className=" w-1/2 text-start">
-                        <h1 className="text-4xl gap-2 my-1">24</h1>
-                        <h1 className="text-lg"> Products</h1>
-                    </div>
-                </div>
-            
-           
-                <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-yellow-200 flex text-center items-center ">
-                    <div className="w-1/2 ps-14">
-                        <p className="text-6xl mt-3"><MdHome></MdHome></p>
-                    </div>
-                    <div className=" w-1/2 text-start">
-                        <h1 className="text-4xl gap-2 my-1">12</h1>
-                        <h1 className="text-lg"> Shop</h1>
-                    </div>
-                </div>
-          
-           
-                <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-blue-200 flex text-center items-center ">
-                    <div className="w-1/2 ps-14">
-                        <p className="text-5xl mt-3"><FaPhone></FaPhone></p>
-                    </div>
-                    <div className=" w-1/2 text-start">
-                        <h1 className="text-4xl gap-2 my-1">03</h1>
-                        <h1 className="text-lg"> Contact</h1>
-                    </div>
-                </div>
-                </div>
+  return (
+    <div>
+      <div className="flex items-center ">
+        <h2 className="me-3 text-2xl">Hi, Welcome </h2>
+        <p className="text-4xl capitalize text-orange-600">{user?.displayName ? user.displayName : ""}</p>
+      </div>
 
+      <div className="flex gap-3 my-5">
+        <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-purple-200 flex text-center items-center ">
+          <div className="w-1/2 ps-14">
+            <p className="text-5xl mt-3">
+              <FaAddressBook />
+            </p>
+          </div>
+          <div className=" w-1/2 text-start">
+            <h1 className="text-4xl gap-2 my-1">24</h1>
+            <h1 className="text-lg"> Products</h1>
+          </div>
         </div>
 
-    );
+        <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-yellow-200 flex text-center items-center ">
+          <div className="w-1/2 ps-14">
+            <p className="text-6xl mt-3">
+              <MdHome></MdHome>
+            </p>
+          </div>
+          <div className=" w-1/2 text-start">
+            <h1 className="text-4xl gap-2 my-1">12</h1>
+            <h1 className="text-lg"> Shop</h1>
+          </div>
+        </div>
+
+        <div className="shadow-lg w-72 h-32 p-2 mb-3  bg-blue-200 flex text-center items-center ">
+          <div className="w-1/2 ps-14">
+            <p className="text-5xl mt-3">
+              <FaPhone></FaPhone>
+            </p>
+          </div>
+          <div className=" w-1/2 text-start">
+            <h1 className="text-4xl gap-2 my-1">03</h1>
+            <h1 className="text-lg"> Contact</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-5">
+        <div>
+          <span className="countdown font-mono text-4xl">{padZero(countdown.days)}</span>
+          days
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">{padZero(countdown.hours)}</span>
+          hours
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">{padZero(countdown.minutes)}</span>
+          min
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">{padZero(countdown.seconds)}</span>
+          sec
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserDashboard;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,41 @@ import Hero from "./Hero";
 import Popular from "./PopularProduct/Popular";
 import pic from '../../assets/offer1.jpg'
 import pic1 from '../../assets/newsletter.png'
+import { useEffect, useState } from "react";
+
+function padZero(number) {
+    return number < 10 ? `0${number}` : number;
+}
 const Home = () => {
+    const [countdown, setCountdown] = useState({ days: 10, hours: 9, minutes: 3, seconds: 0 });
+
+    useEffect(() => {
+        const countdownInterval = setInterval(() => {
+            setCountdown(prevCountdown => {
+                let { days, hours, minutes, seconds } = prevCountdown;
+                seconds--;
+                if (seconds < 0) {
+                    seconds = 59;
+                    minutes--;
+                    if (minutes < 0) {
+                        minutes = 59;
+                        hours--;
+                        if (hours < 0) {
+                            hours = 23;
+                            days--;
+                            if (days < 0) {
+                                clearInterval(countdownInterval);
+                                return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+                            }
+                        }
+                    }
+                }
+                return { days, hours, minutes, seconds };
+            });
+        }, 1000);
+
+        return () => clearInterval(countdownInterval);
+    }, []);
     return (
         <div>
             <Hero></Hero>
@@ -15,29 +49,42 @@ const Home = () => {
                     <div className="py-2">
                         <h1 className="md:text-5xl text-2xl font-bold mb-4">Deal Of the day</h1>
                         <p className="">Provident cupiditate voluptatem et in. Quaerat <br /> fugiat ut assumenda excepturi exercitationem quasi. <br />In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <div className="my-6 flex gap-4">
-                            <div className="w-25 text-center bg-white p-4 rounded-lg font-bold text-2xl">
-                                <p>04</p>
-                                <p className="px-2">Days</p>
+
+
+                        <div className=" md:w-1/2 flex gap-5 my-7">
+                            <div className="bg-white p-3 pt-4 text-center rounded-lg font-serif text-2xl">
+                                <span className="countdown font-mono text-4xl ">{padZero(countdown.days)}</span>
+                                <p>days</p>
                             </div>
-                            <div className="w-25 text-center bg-white p-4 rounded-lg font-bold text-2xl">
-                                <p>09</p>
-                                <p>Hours</p>
+                            <div className="bg-white p-2 pt-4 text-center rounded-lg font-serif text-2xl">
+                                <span className="countdown font-mono text-4xl ">{padZero(countdown.hours)}</span>
+                                <p>hours</p>
                             </div>
-                            <div className="w-25 text-center bg-white p-4 rounded-lg font-bold text-2xl">
-                                <p>03</p>
-                                <p>Minute</p>
+                            <div className="bg-white p-4 text-center rounded-lg font-serif text-2xl">
+                                <span className="countdown font-mono text-4xl ">{padZero(countdown.minutes)}</span>
+                                <p>min</p>
                             </div>
+                            <div className="bg-white p-4 text-center rounded-lg font-serif text-2xl">
+                                <span className="countdown font-mono text-4xl ">{padZero(countdown.seconds)}</span>
+                                <p>sec</p>
+                            </div>
+
+                           
                         </div>
+
+                       
                         <button className="btn bg-black border-black text-white hover:text-black px-8">Shop Now</button>
                     </div>
                 </div>
             </div>
+
+
+
             {/* Newsletter  */}
             <div>
                 <div className="hero shadow-xl my-4  w-full" >
                     <div className=" lg:flex  lg:justify-around" >
-                        <img src={pic1} className="lg:w-1/3  rounded-2xl h-80"  />
+                        <img src={pic1} className="lg:w-1/3  rounded-2xl h-80" />
                         <div className="py-2 px-4 mt-12">
                             <h1 className="md:text-4xl text-2xl font-bold mb-4">Subscribe For Online Shopping Update</h1>
                             <p className="">Provident cupiditate voluptatem et in. Quaerat <br /> fugiat ut assumenda excepturi exercitationem quasi. </p>
